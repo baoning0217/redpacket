1cf52e5179f3c4386ab22775b1ec9b581f7f1eb3
captainbn <bnwyyx217@163.com>
2019-07-09 23:52:30 +0800

    添加fastjson配置

--- src/main/java/com/xishanqu/redpacket/config/WebConfig.java
+++ src/main/java/com/xishanqu/redpacket/config/WebConfig.java
@@ -1,10 +1,17 @@
 package com.xishanqu.redpacket.config;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.nio.charset.Charset;
+import java.util.List;
 import java.util.concurrent.Executor;
 
 /**
@@ -12,7 +19,7 @@ import java.util.concurrent.Executor;
  */
 @Configuration
 @EnableWebMvc
-public class WebConfig extends AsyncConfigurerSupport {
+public class WebConfig extends AsyncConfigurerSupport implements WebMvcConfigurer {
 
     @Override
     public Executor getAsyncExecutor() {
@@ -23,5 +30,29 @@ public class WebConfig extends AsyncConfigurerSupport {
         taskExecutor.initialize();
         return taskExecutor;
     }
+
+
+    /**
+     * 配置fastJson
+     * @param converters
+     */
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+        FastJsonConfig config = new FastJsonConfig();
+        config.setDateFormat("yyyy-MM-dd");
+        config.setCharset(Charset.forName("UTF-8"));
+        config.setSerializerFeatures(
+                SerializerFeature.WriteClassName,
+                SerializerFeature.WriteMapNullValue,
+                SerializerFeature.PrettyFormat,
+                SerializerFeature.WriteNullListAsEmpty,
+                SerializerFeature.WriteNullStringAsEmpty
+        );
+        converter.setFastJsonConfig(config);
+        converters.add(converter);
+    }
+
+
 
 }
