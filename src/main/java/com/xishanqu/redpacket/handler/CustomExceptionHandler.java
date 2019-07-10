ad1d8b8502a9b9df48d827ea65051d14dd0aed0c
captainbn <bnwyyx217@163.com>
2019-07-11 01:06:41 +0800

    ControllerAdvice注解

---
+++ src/main/java/com/xishanqu/redpacket/handler/CustomExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.xishanqu.redpacket.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+
+/**
+ * @Function:
+ * @Author: BaoNing
+ * @Date: 2019-07-11
+ */
+@ControllerAdvice
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ModelAndView uploadException(MaxUploadSizeExceededException e) throws IOException {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("msg","上传文件大小超出限制");
+        modelAndView.setViewName("error");
+        return modelAndView;
+    }
+
+}
